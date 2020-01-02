1ad0a0058aab76f7c5985513635992cd9d015acd
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2020-01-02 15:24:28 +0100

    Shorten code Update constants names to be more clear pipe when previous command was a pipe command

--- microshell/test.sh
+++ microshell/test.sh
@@ -11,14 +11,16 @@ test_line () {
 		printf "\e[0;31mLEAKS\n\e[0m"
 	fi
 	pid=$( pgrep microshell )
+	printf "\e[0;31m"
 	lsof -c microshell | grep $pid | grep -v cwd | grep -v txt | grep -v 0r | grep -v 1w | grep -v 2u | grep microshel
+	printf "\e[0m"
 	kill -9 $pid
 	wait $pid 2>/dev/null
 	#cat -e out.res > out
 }
 
 printf "\e[1;32mCompile\n"
-gcc -g -Wall -Werror -Wextra microshell.c -o microshell
+gcc -g -Wall -Werror -Wextra -DTEST_SH microshell.c -o microshell
 printf "\e[1;36mTest\n\e[0m"
 rm -f out.res leaks.res out
 test_line /bin/ls
